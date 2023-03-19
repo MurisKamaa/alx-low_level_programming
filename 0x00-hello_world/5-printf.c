a4c9c9f1fce99e0f82d36169bfb75ae1183fa905
Paul Kamau <engpaulkamau@gmail.com>
2023-03-19 06:07:12 -0700

    Added the c file 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,9 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-}
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+{
+	char myStr[] = "with proper grammar, but the outcome is a piece of art,";
+	
+	printf("%s\n", myStr);
 	return (0);
 }
