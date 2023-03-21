eb6501f71c08ac28f3770961c579e0161c80ce97
Paul Kamau <engpaulkamau@gmail.com>
2023-03-21 01:07:53 -0700

    Added the script 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-        int i = 0;
-
-        while (i < 10)
-        {
-                printf("%d", i);
-                i++;
-        }
-        putchar('\n');
-        return (0);
+	int i = 0;
+	
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
