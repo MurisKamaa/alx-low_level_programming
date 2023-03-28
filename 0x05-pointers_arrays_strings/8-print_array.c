84705241c14a6b88240f92f767111ff6b939ef83
Paul Kamau <engpaulkamau@gmail.com>
2023-03-28 07:59:14 -0700

    Created the function 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_array - prints n elements of an array of integers
+ *@a: input a, array
+ *@n: input n, number of elements to be printed 
+ *Return: no return
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(",");
+	}
+	printf("\n");
+}
