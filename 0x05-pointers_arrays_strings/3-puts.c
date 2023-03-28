9179e0eb285405f84a8c7e3528209547849cedd6
Paul Kamau <engpaulkamau@gmail.com>
2023-03-28 02:13:12 -0700

    Created the function 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,8 +8,8 @@ void _puts(char *str)
 {
 	for (int i = 0; i < strlen(*str); i++)
 	{
-		putchar(str[i]);
-		if (str[i] == '\0')
+		putchar(*str[i]);
+		if (*str[i] == '\0')
 			break;
 	}
 	putchar('\n');
