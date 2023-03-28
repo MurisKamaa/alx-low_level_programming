363aebd1d8be95294d28574e3449d7b5daed1e17
Paul Kamau <engpaulkamau@gmail.com>
2023-03-28 02:08:55 -0700

    Created the function 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_puts - prints a string followed by a new line to stdout
+ *@str: input string
+ *Return: no return
+ */
+void _puts(char *str)
+{
+	for (int i = 0; i < strlen(*str); i++)
+	{
+		putchar(str[i]);
+		if (str[i] == '\0')
+			break;
+	}
+	putchar('\n');
+}
