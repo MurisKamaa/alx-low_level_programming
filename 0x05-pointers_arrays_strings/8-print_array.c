b26adfcfeb22180f76e507f696d915afd4ce5964
Paul Kamau <engpaulkamau@gmail.com>
2023-03-28 08:27:53 -0700

    Removed trailing white space in 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,7 @@
 /**
  *print_array - prints n elements of an array of integers
  *@a: input a, array
- *@n: input n, number of elements to be printed 
+ *@n: input n, number of elements to be printed
  *Return: no return
  */
 void print_array(int *a, int n)
