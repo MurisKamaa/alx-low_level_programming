427971e80a792bb42835f3e53084d1c7a675ad74
Paul Kamau <engpaulkamau@gmail.com>
2023-04-02 06:07:32 -0700

    Edited the script 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *_strncpy: copies a string into another
- *@dest: points to destination string
- *@src: points to source string
- *@n: Number of bytes taken from src
- *Return: Returns pointer to destination string
+ * _strncpy - copies a string
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
 		dest[i] = src[i];
-	}
 	for ( ; i < n; i++)
 		dest[i] = '\0';
+
 	return (dest);
 }
