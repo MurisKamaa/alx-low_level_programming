13a90e9bd728cd51861e39b777725f82fa9ae5d8
Paul Kamau <engpaulkamau@gmail.com>
2023-04-06 02:56:32 -0700

    ISO C90 compliance

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,14 +10,14 @@
 int is_palindrome(char *s)
 {
 	int length = strlen(s);
+	int palCheck;
 
 	if (length <= 1)
 		return (1);
 	if (s[0] != s[length - 1])
 		return (0);
 	s[length - 1] = '\0';
-	int palCheck = is_palindrome(s + 1);
-
+	palCheck = is_palindrome(s + 1);
 	s[length - 1] = s[0];
 	return (palCheck);
 }
