8dac8adb82e7297d345a78bb8fadedb920665524
Paul Kamau <engpaulkamau@gmail.com>
2023-04-14 10:24:51 -0700

    More dits in the script

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+C_FILES=$(find . -maxdepth 1 -type f -name "*.c")
+for file in $C_FILES; do
+	gcc -c $file -o ${file%.c}.o
+	ar rcs liball.a ${file%.c}.o
+done
