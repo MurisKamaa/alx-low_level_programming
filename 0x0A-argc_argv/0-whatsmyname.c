6f1add953c58b8fe24fa6a2d2f8d112f73bc872e
Paul Kamau <engpaulkamau@gmail.com>
2023-04-14 15:04:29 -0700

    First commit in argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,3 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -8,7 +6,7 @@
  *@argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
