5f1203c45da9df6191e5b6bd817e970159b8dee1
Paul Kamau <engpaulkamau@gmail.com>
2023-05-12 13:40:23 -0700

    First commit in file I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,40 @@
 #include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters) {
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
+	
 	if (filename == NULL) {
 		return 0;
 	}
 
-	int fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1) {
 		return 0;
 	}
 
-	char *buffer = (char *) malloc(letters + 1);
+	buffer = (char *) malloc(letters + 1);
 	if (buffer == NULL) {
 		close(fd);
 		return 0;
 	}
 
-	ssize_t bytes_read = read(fd, buffer, letters);
+	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1) {
 		close(fd);
 		free(buffer);
 		return 0;
 	}
 
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read) {
 		close(fd);
 		free(buffer);
