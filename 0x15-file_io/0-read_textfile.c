5599c1a139e00b2f9cd6ac37ff9bb282b21a4090
Paul Kamau <engpaulkamau@gmail.com>
2023-05-12 13:47:00 -0700

    Betty compliance edit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,41 +7,38 @@
  *
  * Return: numbers of letters printed. It fails, returns 0.
  */
-ssize_t read_textfile(const char *filename, size_t letters) {
+ssize_t read_textfile(const char *filename, size_t letters)
+{
 	int fd;
 	ssize_t bytes_read, bytes_written;
 	char *buffer;
-	
-	if (filename == NULL) {
-		return 0;
-	}
 
+	if (filename == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd == -1) {
-		return 0;
-	}
-
+	if (fd == -1)
+		return (0);
 	buffer = (char *) malloc(letters + 1);
-	if (buffer == NULL) {
+	if (buffer == NULL)
+	{
 		close(fd);
-		return 0;
+		return (0);
 	}
-
 	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1) {
+	if (bytes_read == -1)
+	{
 		close(fd);
 		free(buffer);
-		return 0;
+		return (0);
 	}
-
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read) {
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
 		close(fd);
 		free(buffer);
-		return 0;
+		return (0);
 	}
-
 	close(fd);
 	free(buffer);
-	return bytes_written;
+	return (bytes_written);
 }
